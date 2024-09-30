f0d63a1fd51b02ea87a324c3f6e0d5a51a2e68e9
podusit <d.rimdusit@gmail.com>
2024-09-30 13:24:15 +0700

    hackhour:JavaScript Values Variables Assignment ex1-4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let name = "Karter"
+let sirname = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+
+const restaurant = "Chef's Table by Chef James"
+const totalNet = 15000
+const openingShop = 1500
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+totalNet = 5000
+openingShop = 2500
